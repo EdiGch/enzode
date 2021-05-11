0f406873b2f050d7e0456d6c4b8057cd6541926c
Grzegorz Charkiewicz <gcharkiewicz@enzode.com>
2021-05-11 12:20:10 +0200

    Funkcje z parametrami opcjonalnymi

--- Dzien01/01-funkcje.py
+++ Dzien01/01-funkcje.py
@@ -14,3 +14,21 @@ def oblicz_pole_kola(r: float) -> float:
     return math.pi * (r ** 2)
 
 print(oblicz_pole_kola("2.0"))
+
+
+# funkcje z parametrami opcjonalnymi
+def create_employee(fname, lname, phone="221234567", email="hello@company.com"):
+    return {
+        "fname": fname, "lname": lname,
+        "phone": phone, "email": email
+    }
+
+"""
+print(create_employee("Marek", "Markiewicz", "606123456", "gmarkiewicz@wick.com"))
+print(create_employee("Marek", "Markiewicz"))
+print(create_employee("Marek", "Markiewicz", email="gmarkiewcz2@wick.com"))
+print(create_employee("Marek", "Markiewicz", phone="500478965"))
+print(create_employee("Marek", "Markiewicz", "500478965"))
+print("Hello", "world!")
+print("Hello", "world!", sep=" | ")
+print("Hello", "world!", sep=" | ", end=" **KONIEC LINI") """
